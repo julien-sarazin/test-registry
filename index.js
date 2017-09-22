95b5cdc7ae6f1d0220ca40647f196cd44c71fcc4
Julien Sarazin <julien.sarazin@me.com>
2017-09-22 11:39:11 +0200

    updated sources

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const registry = require('yemma');
 
 registry
   .on(registry.events.started, app => console.log('Registry started on port', app.settings.port))
+  .on(registry.events.init.settings, app => settings => settings.port = process.env.PORT)
   .start();
